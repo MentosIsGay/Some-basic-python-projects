b19199f66cf2f45030f3c2aaaec79fff9f6f5e67
SpicyMentos <98723664+MentosIsGay@users.noreply.github.com>
2022-01-31 19:43:00 -0800

    PyLibs
    an incredibly simple mad-libs. made in 20 seconds for fun

---
+++ A Epic Madlibs.py
@@ -0,0 +1,8 @@
+print("Welcome to MadLibs! Answer the statments below and make your own story!")
+
+name = input("Enter your name: ")
+animal = input("Enter the name of an animal: ")
+place = input("Enter a famous place: ")
+food = input("Enter a type of food: ")
+
+print(name + " and a/an " + animal + " went to " + place + " and ate " + food + ".")
